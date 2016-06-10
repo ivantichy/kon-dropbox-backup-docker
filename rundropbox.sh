57ba1fd16c440559a72f37c4e2b66f52bb555be0
Ivan Tichy <ivan.tichy@gmail.com>
2016-06-10 14:51:26 +0200

    naming container

--- rundropbox.sh
+++ rundropbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -h dropbox --name dropbox -v /var/docker-data/Dropbox/config:/root/.dropbox -v /var/docker-data/Dropbox/zaloha:/root/Dropbox -v /var/docker-data/:/var/docker-data/ -v /etc/localtime:/etc/localtime:ro ivantichy/kon-dropbox-backup
+docker run -d -h dropbox --name kon-dropbox-backup  -v /var/docker-data/Dropbox/config:/root/.dropbox -v /var/docker-data/Dropbox/zaloha:/root/Dropbox -v /var/docker-data/:/var/docker-data/ -v /etc/localtime:/etc/localtime:ro ivantichy/kon-dropbox-backup
 
 
 
