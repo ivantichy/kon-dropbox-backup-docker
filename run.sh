6dbec354e06954743dc6192f52c55ce8ce40e8c0
Ivan Tichy <ivan.tichy@gmail.com>
2016-06-10 14:45:50 +0200

    cleanup

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker run -d -h dropbox --name dropbox -v /var/docker-data/Dropbox/config:/root/.dropbox -v /var/docker-data/Dropbox/zaloha:/root/Dropbox -v /var/docker-data/:/var/docker-data/ -v /etc/localtime:/etc/localtime:ro ivantichy/kon-dropbox-backup
-
+./rundropbox.sh
 
